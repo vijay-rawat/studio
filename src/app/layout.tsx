5e4c9352d4422c00c67f7eafc1e239d42f51855a
Vijay Rawat <vijay.rawat@cloudanalogy.com>
2025-05-22 13:14:44 +0000

    speed analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from 'next';
 import {Geist, Geist_Mono} from 'next/font/google';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} font-sans`}>
         {children}
         <Toaster />
+        <SpeedInsights />
       </body>
     </html>
   );
